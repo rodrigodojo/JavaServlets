8595274d187c015b7b914b3952f010bf1c3e5017
rodrigodojo <rodrigodoj@gmail.com>
2021-12-28 21:30:37 -0300

    Oi mundo Servlet

---
+++ src/main/java/servlet/OiMundoServlet.java
@@ -0,0 +1,26 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/oi")
+public class OiMundoServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("oi mundo !");
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
