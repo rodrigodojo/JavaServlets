11b1188789a8633c83c331f92f65f026c42c4e83
rodrigodojo <rodrigodoj@gmail.com>
2022-01-06 22:25:37 -0300

    novo servlet criado

---
+++ src/main/java/servlet/NovaEmpresaServlet.java
@@ -0,0 +1,23 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class NovaEmpresaServlet
+ */
+public class NovaEmpresaServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Cadastrando nova empresa");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>Empresa cadastrada com sucesso!</body></html>");
+	}
+
+}
